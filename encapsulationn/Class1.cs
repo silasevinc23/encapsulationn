137f8aae891e63c22715c04eb3ff183f45778a27
sila7 <sila7@SILA>
2024-08-20 10:16:32 +0300

    Add project files.

---
+++ encapsulationn/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+
+        public class Araba
+        {
+            public string marka;
+            public string model;
+            public string renk;
+            private int kapı_sayısı;
+
+
+            public int Kapı_Sayısı
+            {
+                get { return kapı_sayısı; }
+                set
+                {
+                    if (value != 2 && value != 4)
+                    {
+                        Console.WriteLine("Geçerli olmayan kapı sayısı girdiniz!");
+                        kapı_sayısı = -1;
+
+                    }
+                    else kapı_sayısı = value;
+
+
+
+                }
+
+            }
+
+
+
+        }
+
+
+  
+}
+
---
+++ encapsulationn/Program.cs
@@ -0,0 +1,4 @@
+﻿using encapsulation;
+Araba araba1= new Araba();
+araba1.Kapı_Sayısı = 5;
+Console.WriteLine(araba1.Kapı_Sayısı);
